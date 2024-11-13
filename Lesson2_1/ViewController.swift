fb034a15eeaac863092ba940302718b70d260e5a
Evgeny Mastepan <phenikomperekt@gmail.com>
2024-11-13 10:29:34 +0700

    Lesson2_1 is began

---
+++ Lesson2_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson2_1
+//
+//  Created by Evgeny Mastepan on 13.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
